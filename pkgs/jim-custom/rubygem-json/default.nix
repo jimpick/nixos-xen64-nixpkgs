9db99339342e5272b2ccbeb7cd67f5da4e346644
Jim Pick <jim@jimpick.com>
2009-03-02 14:04:21 -0800

    Fix URL.

--- pkgs/jim-custom/rubygem-json/default.nix
+++ pkgs/jim-custom/rubygem-json/default.nix
@@ -19,7 +19,8 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     #url = mirror://gnu/hello/hello-2.3.tar.bz2;
-    url = file:///Library/Ruby/Gems/1.8/cache/json-1.1.3.gem;
+    #url = file:///Library/Ruby/Gems/1.8/cache/json-1.1.3.gem;
+    url = http://gems.rubyforge.org/gems/json-1.1.3.gem;
     sha256 = "379852d4a88e68b58605a3fbad5c6d7c329a4e7e91b221a3d4b077c63cdc64fe";
   };
   
